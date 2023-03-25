c1ba0d8d9f935d656b054bea9f24e3d35e1b0deb
Murat Altun <mraltun@gmail.com>
2023-03-25 15:44:04 +0300

    NavLinks have active check now. Added open/close navbar in responsive desing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 
 const Header = () => {
+  const [hidden, setHidden] = useState(true);
+  const activeClass =
+    "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
+  const inActiveClass =
+    "text-base block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
   return (
     <header>
       <nav className='bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
         <div className='container flex flex-wrap items-center justify-between mx-auto'>
           <Link to='/' className='flex items-center'>
             <img src={Logo} className='h-6 mr-3 sm:h-9' alt='MovieApp Logo' />
-            <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
+            <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
               MovieApp
             </span>
           </Link>
@@ -20,6 +26,7 @@ const Header = () => {
               aria-controls='navbar-search'
               aria-expanded='false'
               className='md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1'
+              onClick={() => setHidden(!hidden)}
             >
               <svg
                 className='w-5 h-5'
@@ -58,6 +65,7 @@ const Header = () => {
                 id='search-navbar'
                 className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Search...'
+                autoComplete='off'
               />
             </div>
             <button
@@ -66,6 +74,7 @@ const Header = () => {
               className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
               aria-controls='navbar-search'
               aria-expanded='false'
+              onClick={() => setHidden(!hidden)}
             >
               <span className='sr-only'>Open menu</span>
               <svg
@@ -84,7 +93,9 @@ const Header = () => {
             </button>
           </div>
           <div
-            className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
+            className={`${
+              hidden ? "hidden" : ""
+            } items-center justify-between  w-full md:flex md:w-auto md:order-1`}
             id='navbar-search'
           >
             <div className='relative mt-3 md:hidden'>
@@ -108,13 +119,16 @@ const Header = () => {
                 id='search-navbar'
                 className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                 placeholder='Search...'
+                autoComplete='off'
               />
             </div>
             <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
                 <NavLink
                   to='/'
-                  className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }
                   end
                 >
                   Home
@@ -123,7 +137,10 @@ const Header = () => {
               <li>
                 <NavLink
                   to='/movies/popular'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }
+                  end
                 >
                   Popular
                 </NavLink>
@@ -131,7 +148,10 @@ const Header = () => {
               <li>
                 <NavLink
                   to='/movies/top'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }
+                  end
                 >
                   Top
                 </NavLink>
@@ -139,7 +159,10 @@ const Header = () => {
               <li>
                 <NavLink
                   to='/movies/upcoming'
-                  className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
+                  className={({ isActive }) =>
+                    isActive ? activeClass : inActiveClass
+                  }
+                  end
                 >
                   Upcoming
                 </NavLink>
