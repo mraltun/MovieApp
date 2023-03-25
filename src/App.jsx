688c3d44db41caddefa0683c3dbffb330fbbba53
Murat Altun <mraltun@gmail.com>
2023-03-25 14:45:08 +0300

    Added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { Header } from "./components";
+import { Footer, Header } from "./components";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className=' bg-white shadow m-4 dark:bg-gray-800'>
+      <div className='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between'>
+        <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
+          © 2023{" "}
+          <Link to='/' className='hover:underline'>
+            MovieApp
+          </Link>
+          . All Rights Reserved.
+        </span>
+        <ul className='flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0'>
+          <li>
+            <a
+              href='#'
+              target='_blank'
+              className='mr-4 hover:underline md:mr-6 '
+            >
+              Instagram
+            </a>
+          </li>
+          <li>
+            <a
+              href='#'
+              target='_blank'
+              className='mr-4 hover:underline md:mr-6'
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href='#'
+              target='_blank'
+              className='mr-4 hover:underline md:mr-6'
+            >
+              Linkedin
+            </a>
+          </li>
+          <li>
+            <a href='#' target='_blank' className='hover:underline'>
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
