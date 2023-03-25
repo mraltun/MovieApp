425d83d2e352c044f61912262addca6e905ab15b
Murat Altun <mraltun@gmail.com>
2023-03-25 14:11:35 +0300

    Updated Header with NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 
 const Header = () => {
@@ -112,37 +112,37 @@ const Header = () => {
             </div>
             <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
-                <Link
+                <NavLink
                   to='/'
                   className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white'
-                  aria-current='page'
+                  end
                 >
                   Home
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to='/movies/popular'
                   className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 >
                   Popular
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to='/movies/top'
                   className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 >
                   Top
-                </Link>
+                </NavLink>
               </li>
               <li>
-                <Link
+                <NavLink
                   to='/movies/upcoming'
                   className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'
                 >
                   Upcoming
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
