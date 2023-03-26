765b5f69a61527dcde029c5504512d66b745b612
Murat Altun <mraltun@gmail.com>
2023-03-26 16:33:23 +0300

    MovieList fetch movie data and pass to Card

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card } from "../components";
 
 const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        `${apiUrl}/movie/now_playing?api_key=${apiKey}`
+      );
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      setMovies(data.results);
+    };
+    // fetchMovies();
+  }, []);
+
   return (
     <main>
       <section className='max-w-7xl mx-auto py-7'>
         <div className='flex justify-start flex-wrap'>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
         </div>
       </section>
     </main>
