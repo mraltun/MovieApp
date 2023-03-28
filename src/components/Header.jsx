954b6d7c68359ee753ae9880d59c98db1e49404f
Murat Altun <mraltun@gmail.com>
2023-03-28 17:24:40 +0300

    Search box in Header works now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 
 const Header = () => {
@@ -7,10 +7,19 @@ const Header = () => {
   const [darkMode, setDarkMode] = useState(
     JSON.parse(localStorage.getItem("darkMode")) || false
   );
+  const navigate = useNavigate();
   const activeClass =
     "text-base block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white";
   const inActiveClass =
     "text-base block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const queryTerm = e.target.search.value;
+    e.target.reset();
+
+    return navigate(`/search?q=${queryTerm}`);
+  };
 
   useEffect(() => {
     localStorage.setItem("darkMode", JSON.stringify(darkMode));
@@ -109,13 +118,16 @@ const Header = () => {
                 </svg>
                 <span className='sr-only'>Search icon</span>
               </div>
-              <input
-                type='text'
-                id='search-navbar'
-                className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Search...'
-                autoComplete='off'
-              />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type='text'
+                  id='search-navbar'
+                  name='search'
+                  className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='Search...'
+                  autoComplete='off'
+                />
+              </form>
             </div>
             <button
               data-collapse-toggle='navbar-search'
@@ -164,13 +176,16 @@ const Header = () => {
                   ></path>
                 </svg>
               </div>
-              <input
-                type='text'
-                id='search-navbar'
-                className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                placeholder='Search...'
-                autoComplete='off'
-              />
+              <form onSubmit={handleSubmit}>
+                <input
+                  type='text'
+                  id='search-navbar'
+                  name='search'
+                  className='block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  placeholder='Search...'
+                  autoComplete='off'
+                />
+              </form>
             </div>
             <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
               <li>
