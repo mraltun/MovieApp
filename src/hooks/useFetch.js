91a8365fa581baf35803f85cb82b5f5d13a00170
Murat Altun <mraltun@gmail.com>
2023-03-28 17:22:00 +0300

    Search page now has searching and useFetch modified to return search results

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
 
-const useFetch = (apiPath) => {
+const useFetch = (apiPath, queryTerm = "") => {
   const [data, setData] = useState([]);
   const apiUrl = import.meta.env.VITE_API_URL;
   const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `${apiUrl}${apiPath}${apiKey}`;
+  const url = `${apiUrl}${apiPath}${apiKey}&query=${queryTerm}`;
 
   useEffect(() => {
     const fetchMovies = async () => {
